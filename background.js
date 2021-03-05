fad4cfaa69cda697aa6768fb2d0ee9807e0f2a63
fsbakhtiar <farzadsbakhtiar@gmail.com>
2021-03-05 11:04:48 -0800

    We hook into navigation of tabs in windows with sessions and save their urls.

--- background.js
+++ background.js
@@ -1,9 +1,11 @@
-var WINDOW_SESSION_MAPPING = {}
-var SESSION_WINDOW_MAPPING = {}
+var WINDOW_SESSION_MAPPING = {};
+var SESSION_WINDOW_MAPPING = {};
+var SESSION_DATA = {};
+const SESSION_DATA_KEY = 'SESSION-DATA';
 
 chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
-	console.log('MESSAGE received in BACK');
-	console.log(message);
+	//console.log('MESSAGE received in BACK');
+	//console.log(message);
 	switch (message.meta){
 		case 'start session':
 			startSession(message.data.name);
@@ -13,6 +15,53 @@ chrome.runtime.onMessage.addListener(function(message, sender, sendResponse){
 			console.log(message.meta);
 	}
 });
+
+/*chrome.windows.onRemoved.addListener((windowId) => {
+    // check if the window is associated with the session
+    sessionName = getWindowSession(windowId);
+    if(sessionName != null){
+        // get the window instance
+        chrome.windows.get(windowId, {}, (window) => {
+        // save the session's tabs in storage
+            saveSessionWindow(sessionName, window);
+        // can we retrieve a window's tabs after it's been closed??
+            // NOOOOO
+
+        });
+    }
+});*/
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    //console.log('tab updated listener called:');
+    //console.log(changeInfo);
+    // only listen to loading event for now -- this is when a new url is loading
+    if (changeInfo.status != 'loading' || !changeInfo.url) return;
+    session = getSessionForTab(tab);
+    /*console.log('changeInfo', changeInfo);
+    console.log('session for tab:');
+    console.log(session);*/
+    if (!session){
+        // tab is not in a session, nothing to do
+        return;
+    }
+    const newUrl = changeInfo.url;
+    if (newUrl){
+        updateSessionItemUrl(session, tab, newUrl);
+    }
+
+});
+
+
+function getSessionForTab(tab){
+    return getWindowSession(tab.windowId);
+}
+
+function updateSessionItemUrl(sessionName, tab, url){
+    SESSION_DATA[sessionName] = SESSION_DATA[sessionName] || {};
+    SESSION_DATA[sessionName][tab.id] = url;
+   // console.log('SESSION_DATA, session name, tab.id, url');
+   // console.log(SESSION_DATA, sessionName, tab.id, url);
+}
 
 /*chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({color: '#3aa757'}, function() {
@@ -34,7 +83,20 @@ function createSessionWindow(name){
 
 }
 
-
+/*function saveSessionWindow(sessionName, window){
+    var key = 'SESSION-' + sessionName;
+    chrome.storage.local.get(key, (sessionData) => {
+        console.log('Session RETRIEVED');
+        console.log(sessionData);
+        sessionData['window'] = window;
+        chrome.storage.local.set({key: sessionData}, () => {
+            console.log('Session SAVED');
+            console.log(key);
+            console.log(sessionData);
+        });
+    });
+}
+*/
 function mapWindowToSession(window, sessionName){
 	// limitation: only one window for a session
     // the limitation is realized on the double mapping below
@@ -42,12 +104,18 @@ function mapWindowToSession(window, sessionName){
     // the effective one will be the LAST ONE TO BE OPENED
     WINDOW_SESSION_MAPPING[window.id] = sessionName
     SESSION_WINDOW_MAPPING[sessionName] = window.id
-
+    console.log('Mapped window to session. Window session mapping:');
     console.log(WINDOW_SESSION_MAPPING);
 }
 
+function getWindowSession(windowId){
+    console.log('Getting session for window. WINDOW SESSION MAP:')
+    console.log(WINDOW_SESSION_MAPPING, windowId, windowId in WINDOW_SESSION_MAPPING);
+    return windowId in WINDOW_SESSION_MAPPING ? WINDOW_SESSION_MAPPING[windowId] : null;
+}
+
 function switchToWindow(window){
-    chrome.windows.update(window.id, {focused: true}, (window) => {console.log(`changed focus to window ${window.id}`)});
+    chrome.windows.update(window.id, {focused: true}, (window) => {});
 }
 
 function thereIsASession(name, executeThisWithWindow, otherwiseThis){
@@ -60,13 +128,9 @@ function thereIsASession(name, executeThisWithWindow, otherwiseThis){
                 executeThisWithWindow(window);
             }
             else{
+                console.log('EXECUTED WITHOUT WINDOW');
                 otherwiseThis();
             }
         });
     }
-}
-
-
-function switchToSession(name){
-    //	todo
 }
